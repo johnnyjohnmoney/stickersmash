9bd4d8c63297bd565dc6f54d6076e2cee463e952
Emmanuel Orevba <emmaorevba@yahoo.com>
2025-11-11 23:18:42 +0100

    First commit

---
+++ app/index.tsx
@@ -0,0 +1,21 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#25292e",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "#fff",
+  },
+});
